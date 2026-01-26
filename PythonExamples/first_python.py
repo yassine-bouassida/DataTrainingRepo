600c2f3a69088cd94d12dc2725ebcfcfbc235b03
WilliamTRev <william.terry@revature.com>
2026-01-26 15:12:50 -0500

    added python notes examples

---
+++ PythonExamples/first_python.py
@@ -0,0 +1,34 @@
+print("hello world")
+
+my_number: int = 5
+my_string: str = "This is a string"
+my_string_number: float = "This is a string despite saying I expect a float"
+
+print(type(my_number))
+print(type(my_string))
+print(type(my_string_number))
+# The above line will not raise an error, but my_string_number will be of type str, not float.
+
+#numeric datatypes are integers and floats
+my_float: float = 5.99
+print(type(my_float))
+my_integer: int = 5
+
+#string is the data type for text
+my_string: str = "Hello World"
+print(type(my_string))
+
+#booleans can either be True or False
+my_boolean: bool = True
+print(type(my_boolean))
+my_other_boolean: bool = False
+print(type(my_other_boolean))
+#notice the first letter is capitalized for boolean values
+
+
+
+
+
+
+
+
