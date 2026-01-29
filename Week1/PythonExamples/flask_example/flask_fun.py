cc66f5657250cf6bf13edfd8a57a01ce6857a342
WilliamTRev <william.terry@revature.com>
2026-01-29 10:59:09 -0500

    pushed github

--- Week1/PythonExamples/flask_example/flask_fun.py
+++ Week1/PythonExamples/flask_example/flask_fun.py
@@ -1,1 +1,13 @@
 from flask import Flask, request
+
+app: Flask = Flask(__name__) #this tells flask where to look for resources (name is reference to the module it resides in)
+
+count = 0 
+
+data_set = {"1":"some data"}
+
+@app.route("/", methods = ["GET"])
+def hello_world():
+    return "Hello World"
+
+app.run()
