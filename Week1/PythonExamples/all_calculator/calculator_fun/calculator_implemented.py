3536391c3958e22cac3f6e6667904deb91e78bbc
WilliamTRev <william.terry@revature.com>
2026-01-29 10:12:26 -0500

    updated testing

--- Week1/PythonExamples/all_calculator/calculator_fun/calculator_implemented.py
+++ Week1/PythonExamples/all_calculator/calculator_fun/calculator_implemented.py
@@ -18,9 +18,10 @@ class CalculatorImp(Calculator):
     
     def subtract(self, a:float, b:float )-> float:
         if isinstance(a,numbers.Number) and isinstance(b,numbers.Number): # check to make sure input is numeric
-            result = a - b
+            result = a-b
+            return result
         else:
             raise CustomException("One or more entries were not numeric")
     
 calc = CalculatorImp()
-print(calc.add(5,"5"))
+print(calc.add(5,5))
--- Week1/PythonExamples/all_calculator/calculator_fun/testcalc.py
+++ Week1/PythonExamples/all_calculator/calculator_fun/testcalc.py
@@ -1,8 +1,7 @@
-import pytest
-from fun_except import CustomException
 from calculator_implemented import CalculatorImp
 
 calculator = CalculatorImp()
 
 def test_addition_success():
-    result = calculator
+    result = calculator.add(5,5)
+    assert result == 10 #we expect 5+5 to equal 10
