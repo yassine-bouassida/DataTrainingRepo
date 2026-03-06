01c31d5ba7f6c5fe02b2ba800249884f3398b1bc
WilliamTRev <william.terry@revature.com>
2026-03-06 11:11:21 -0500

    simple_dag.py added

--- Week8/Airflow/simple_dag.py
+++ Week8/Airflow/simple_dag.py
@@ -42,7 +42,7 @@ with DAG(
 # export CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
 # # Install Airflow
-# pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+# pip install apache-airflow==${AIRFLOW_VERSION} --constraint ${CONSTRAINT_URL}
 
 
 #could do below but above is better
